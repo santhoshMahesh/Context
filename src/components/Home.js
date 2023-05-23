07eecdb9ee0a9bf3831defa1a8fe9114658258d1
Santhosh Mahesh <santhoshmahesh2001@gmail.com>
2023-05-23 14:38:04 +0530

    SingleProduct UI designing

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import {faker} from '@faker-js/faker'
-import SingleProduct from './SingleProduct'
+import SingleProduct from './SingleProduct.js'
 
 
 const Home = () => {
@@ -18,11 +18,10 @@ const Home = () => {
       <div className='productContainer'>
       
          {  
-            products.map((prod)=>(
-                <SingleProduct prop={prod}/>
+            products.map((product)=>(
+                <SingleProduct prop={product}/>
             ))
-            }
-
+          }
       </div>
   )
 }
--- src/components/SingleProduct.js
+++ src/components/SingleProduct.js
@@ -1,9 +1,14 @@
 import React from 'react'
 
-const SingleProduct = (props) => {
+const SingleProduct = ({prop}) => {
   return (
-    <div>SingleProduct
-
+    <div className='products'>
+      <img src={prop.image} alt={prop.pname}/>
+      <div className='productDesc'>
+        <span style={{fontWeight:700}}>{prop.pname}</span>
+        <span>₹ {prop.price.substring(0,3)}</span>
+      </div>
+      <button className='add'>Add to Cart</button>
     </div>
   )
 }
